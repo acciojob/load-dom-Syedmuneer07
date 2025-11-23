cf05dbac3e7c63c9975e4c84b4d87afaf9c3ee33
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2025-11-23 20:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const para = document.createElement('p');
-	para.innerHTML = "DOM load success";
-	document.body.appendChild(para);
+	
 });
